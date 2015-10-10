605e07991dfbe74363e2dac1a8e45a1e8ec62ebd
max.mrtnv <max.mrtnv@gmail.com>
2015-10-10 10:58:17 +0300

    Implemented navigation view and tab navigation with test fragment

---
+++ app/src/main/java/ru/mrtnv/max/remindme/Constants.java
@@ -0,0 +1,9 @@
+package ru.mrtnv.max.remindme;
+
+public class Constants {
+
+    public static final int TAB_ONE = 1;
+    public static final int TAB_TWO = 1;
+    public static final int TAB_TREE = 1;
+
+}
---
+++ app/src/main/java/ru/mrtnv/max/remindme/adapter/TabsPagerFragmentAdapter.java
@@ -0,0 +1,47 @@
+package ru.mrtnv.max.remindme.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import ru.mrtnv.max.remindme.fragment.ExampleFragment;
+
+public class TabsPagerFragmentAdapter extends FragmentPagerAdapter {
+
+    private String[] tabs;
+
+    public TabsPagerFragmentAdapter(FragmentManager fm) {
+        super(fm);
+
+        tabs = new String[] {
+                "Tab1",
+                "Напоминания",
+                "Tab 1"
+        };
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabs[position];
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch(position){
+            case 0:
+                return ExampleFragment.getInstance();
+
+            case 1:
+                return ExampleFragment.getInstance();
+
+            case 2:
+                return ExampleFragment.getInstance();
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return tabs.length;
+    }
+}
